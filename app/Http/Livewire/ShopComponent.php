e026c7e3cba13d5c7d82e71106b6467715343a29
FranciscoMontenegro97 <francisco.gm97@hotmail.com>
2023-02-11 14:11:43 -0300

    wishlist-components

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -19,8 +19,9 @@ class ShopComponent extends Component
     public $max_value = 1000;
 
     public function store($product_id, $product_name, $product_price){
-        Cart::add($product_id, $product_name, 1, $product_price)->associate('\App\Models\Product');
+        Cart::instance('cart')->add($product_id, $product_name, 1, $product_price)->associate('\App\Models\Product');
         session()->flash('success_message', 'Item added to cart!');
+        $this->emitTo('cart-icon-component', 'refreshComponent');
         return redirect()->route('shop.cart');
     }
 
@@ -32,6 +33,20 @@ class ShopComponent extends Component
         $this->orderBy = $order;
     }
 
+    public function addToWishList($product_id, $product_name, $product_price){
+        Cart::instance('wishlist')->add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
+        $this->emitTo('wish-list-icon-component', 'refreshComponent');
+    }
+
+    public function removeFromWishList($product_id){
+        foreach(Cart::instance('wishlist')->content() as $witem){
+            if($witem->id==$product_id){
+                Cart::instance('wishlist')->remove($witem->rowId);
+                $this->emitTo('wish-list-icon-component', 'refreshComponent');
+                return;
+            }
+        }
+    }
     public function render()
     {
         if($this->orderBy == 'Price: Low to High'){
--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -6,6 +6,13 @@
         nav .hidden{
             display: block;
         }
+        .wishlisted{
+            background-color: #F15412 !important;
+            border: 1px solid transparent !important;
+        }
+        .wishlisted i{
+            color: #fff !important;
+        }
     </style>
 
     <main class="main">
@@ -66,46 +73,54 @@
                             </div>
                         </div>
                         <div class="row product-grid-3">
+                            @php
+                                $witems = Cart::instance('wishlist')->content()->pluck('id');
+                            @endphp
                             @foreach($products as $product)
-                            <div class="col-lg-4 col-md-4 col-6 col-sm-6">
-                                <div class="product-cart-wrap mb-30">
-                                    <div class="product-img-action-wrap">
-                                        <div class="product-img product-img-zoom">
-                                            <a href="{{ route('product.details', ['slug'=>$product->slug])}}">
-                                                <img class="default-img" src="{{ asset('assets/imgs/shop/product-')}}{{$product->id}}-1.jpg" alt="{{$product->name}}">
-                                                <img class="hover-img" src="{{ asset('assets/imgs/shop/product-')}}{{$product->id}}-2.jpg" alt="{{$product->name}}">
-                                            </a>
+                                <div class="col-lg-4 col-md-4 col-6 col-sm-6">
+                                    <div class="product-cart-wrap mb-30">
+                                        <div class="product-img-action-wrap">
+                                            <div class="product-img product-img-zoom">
+                                                <a href="{{ route('product.details', ['slug'=>$product->slug])}}">
+                                                    <img class="default-img" src="{{ asset('assets/imgs/shop/product-')}}{{$product->id}}-1.jpg" alt="{{$product->name}}">
+                                                    <img class="hover-img" src="{{ asset('assets/imgs/shop/product-')}}{{$product->id}}-2.jpg" alt="{{$product->name}}">
+                                                </a>
+                                            </div>
+                                            <div class="product-action-1">
+                                                <a aria-label="Quick view" class="action-btn hover-up" data-bs-toggle="modal" data-bs-target="#quickViewModal">
+                                                    <i class="fi-rs-search"></i></a>
+                                                <a aria-label="Add To Wishlist" class="action-btn hover-up" href="wishlist.php"><i class="fi-rs-heart"></i></a>
+                                                <a aria-label="Compare" class="action-btn hover-up" href="compare.php"><i class="fi-rs-shuffle"></i></a>
+                                            </div>
+                                            <div class="product-badges product-badges-position product-badges-mrg">
+                                                <span class="hot">Hot</span>
+                                            </div>
                                         </div>
-                                        <div class="product-action-1">
-                                            <a aria-label="Quick view" class="action-btn hover-up" data-bs-toggle="modal" data-bs-target="#quickViewModal">
-                                                <i class="fi-rs-search"></i></a>
-                                            <a aria-label="Add To Wishlist" class="action-btn hover-up" href="wishlist.php"><i class="fi-rs-heart"></i></a>
-                                            <a aria-label="Compare" class="action-btn hover-up" href="compare.php"><i class="fi-rs-shuffle"></i></a>
-                                        </div>
-                                        <div class="product-badges product-badges-position product-badges-mrg">
-                                            <span class="hot">Hot</span>
-                                        </div>
-                                    </div>
-                                    <div class="product-content-wrap">
-                                        <div class="product-category">
-                                            <a href="shop.html">Music</a>
-                                        </div>
-                                        <h2><a href="product-details.html">{{$product->name}}</a></h2>
-                                        <div class="rating-result" title="90%">
-                                            <span>
-                                                <span>90%</span>
-                                            </span>
-                                        </div>
-                                        <div class="product-price">
-                                            <span>${{$product->regular_price}} </span>
-                                            <!-- <span class="old-price">$245.8</span> -->
-                                        </div>
-                                        <div class="product-action-1 show">
-                                            <a aria-label="Add To Cart" class="action-btn hover-up" href="#" wire:click.prevent="store({{$product->id}}, '{{$product->name}}', {{$product->regular_price}})"><i class="fi-rs-shopping-bag-add"></i></a>
+                                        <div class="product-content-wrap">
+                                            <div class="product-category">
+                                                <a href="shop.html">Music</a>
+                                            </div>
+                                            <h2><a href="product-details.html">{{$product->name}}</a></h2>
+                                            <div class="rating-result" title="90%">
+                                                <span>
+                                                    <span>90%</span>
+                                                </span>
+                                            </div>
+                                            <div class="product-price">
+                                                <span>${{$product->regular_price}} </span>
+                                                <!-- <span class="old-price">$245.8</span> -->
+                                            </div>
+                                            <div class="product-action-1 show">
+                                                @if($witems->contains($product->id))
+                                                    <a aria-label="Remove From Wishlist" class="action-btn hover-up wishlisted" href="#" wire:click.prevent="removeFromWishList({{$product->id}})"><i class="fi-rs-heart"></i></a>
+                                                @else
+                                                    <a aria-label="Add To Wishlist" class="action-btn hover-up" href="#" wire:click.prevent="addToWishList({{$product->id}},'{{$product->name}}',{{$product->regular_price}})"><i class="fi-rs-heart"></i></a>
+                                                @endif
+                                                <a aria-label="Add To Cart" class="action-btn hover-up" href="#" wire:click.prevent="store({{$product->id}}, '{{$product->name}}', {{$product->regular_price}})"><i class="fi-rs-shopping-bag-add"></i></a>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
                             @endforeach
                         </div>
                         <!--pagination-->
