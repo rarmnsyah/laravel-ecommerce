14538311c5507e2cd32ec8219950408afbf12819
FranciscoMontenegro97 <francisco.gm97@hotmail.com>
2023-02-13 22:26:50 -0300

    edit-and-delete-prodcut

--- resources/views/livewire/admin/admin-product-component.blade.php
+++ resources/views/livewire/admin/admin-product-component.blade.php
@@ -62,7 +62,8 @@
                                                 <td>{{$product->category->name}}</td>
                                                 <td>{{$product->created_at}}</td>
                                                 <td>
-                                                    
+                                                    <a href="{{ route('admin.product.edit', ['product_id'=>$product->id])}}" class="text-info">Edit</a>
+                                                    <a href="#" onclick="deleteConfirmation({{$product->id}})" class="text-danger">Delete</a>
                                                 </td>
                                             </tr>
                                         @endforeach
@@ -77,3 +78,30 @@
         </section>
     </main>
 </div>
+<div class="modal" id="deleteConfirmation">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body pb-30 pt-30">
+                <div class="col-md-12 text-center">
+                    <h4 class="pb-3">Do you want delete this record?</h4>
+                    <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#deleteConfirmation">Cancel</button>
+                    <button type="button" class="btn btn-danger" onclick="deleteProduct()">Delete</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@push('scripts')
+        <script>
+            function deleteConfirmation(id){
+                @this.set('product_id', id);
+                $('#deleteConfirmation').modal('show');
+            }
+
+            function deleteProduct(){
+                @this.call('deleteProduct');
+                $('#deleteConfirmation').modal('hide');
+            }
+        </script>
+@endpush
